6eb53fda94df73168ee110bbee86c41e86d5fa1d
Sparsh Singhal <sparshsinghal15@gmail.com>
2020-08-03 17:51:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  submissions = [
+    {
+      event: 'Synchronicity',
+      links: ['http://www.google.com', '', '', ''],
+    },
+    {
+      event: 'Junoon',
+      links: [
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+      ],
+    },
+    {
+      event: 'DJWAR',
+      links: [
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+      ],
+    },
+    {
+      event: 'DJWAR',
+      links: [
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+      ],
+    },
+    {
+      event: 'DJWAR',
+      links: [
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+        'http://www.google.com',
+      ],
+    },
+  ];
   title = 'dashboard2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
